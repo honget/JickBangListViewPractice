af367c1c319864dab3ba0051e94d1b39fd229a99
Hong <bilre84@naver.com>
2019-04-21 14:48:32 +0900

    리스트와 어뎁터 메인에서 연결

--- app/src/main/java/com/example/jickbanglistviewpractice/MainActivity.java
+++ app/src/main/java/com/example/jickbanglistviewpractice/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.jickbanglistviewpractice;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.jickbanglistviewpractice.Adapters.RoomAdapter;
+import com.example.jickbanglistviewpractice.databinding.ActivityMainBinding;
+import com.example.jickbanglistviewpractice.datas.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Room> roomList;
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        fillData();
+
+        RoomAdapter roomAdapter = new RoomAdapter(MainActivity.this, roomList);
+        act.roomList.setAdapter(roomAdapter);
+
+
+    }
+
+    void fillData(){
+
+        roomList = new ArrayList<Room>();
+//        Room(int deposit, int monthlyRent, int floor, String addr, String roomType, String roomInfo, String imgSrc) {
+
+        roomList.add(new Room(100, 13, 2, "경상북도 구미시 송정동", "분리형 원롬", "(풀옵션 초대박)풀옵션 초대박풀옵션 초대박풀옵션 초대박", null));
+        roomList.add(new Room(1300, 15, 3, "서울시 강남구 강남로", " 원롬", "초대박초대박초대박초대박초대박", null));
+        roomList.add(new Room(1300, 20, 5, "경상북도 구미시 신평동", "분리형 원롬", "초대박", null));
+        roomList.add(new Room(200, 40, 2, "경상북도 구미시 원평동", " 원롬", "풀옵션풀옵션풀옵션풀옵션풀옵션", null));
+        roomList.add(new Room(300, 20, 1, "서울시 나동 송정동", "분리형 원롬", "풀옵션", null));
+        roomList.add(new Room(1200, 14, 6, "경상남도 가시 가동", "분리형 원롬", "(풀옵션1 초대박)", null));
+        roomList.add(new Room(10400, 100, 20, "경상북도 구미시 송정동", "쓰리롬", "(풀옵션2 초대박)", null));
+        roomList.add(new Room(10000, 133, 22, "경상북도 구미시 송정동", "원롬", "(풀옵션3 초대박)", null));
+        roomList.add(new Room(2400, 10, 4, "경상북도 구미시 송정동", "분리형 원롬", "(풀옵션4 초대박)", null));
+        roomList.add(new Room(200, 5, 1, "경상북도 구미시 송정동", " 원롬", "(풀옵션5 초대박)", null));
+        roomList.add(new Room(100, 3, -1, "경상북도 구미시 송정동", "분리형 원롬", "(풀옵션6 초대박)", null));
     }
 }
